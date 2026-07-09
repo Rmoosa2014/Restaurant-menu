0d4bdbf8f3bcb75b17dd9f37e37bbf6abb16d9af
Rmoosa2014 <riyasmosa@gmail.com>
2026-07-09 20:46:05 +0300

    Create sw.js

---
+++ Templates/demo-starter/sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'restaurant-menu-v3'; // Change version to force an update!
+
+const URLS_TO_CACHE = [
+  '/Restaurant-menu/',
+  '/Restaurant-menu/index.html',
+  '/Restaurant-menu/manifest.json',
+  '/Restaurant-menu/logo.png'
+  // Add any other CSS or JS files here if you have them
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(URLS_TO_CACHE);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Clear old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
